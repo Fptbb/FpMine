30876dbb0e66ea9c6af0d80188faef8b0ed2fba4
Lucas Oliveira (fptbb) <lucas@fptbb.com>
2021-11-22 11:12:57 -0300

    Renomeando um arquivo porque o hasted reclama mas é preguiçoso, sim, esse commit é inutil

--- src/components/CraftingTable.js
+++ src/components/CraftingTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './MinecraftCraftingTable.module.css'
+import styles from './CraftingTable.module.css'
 function Item(id) {
   if (!id || id === '') {
     return (
