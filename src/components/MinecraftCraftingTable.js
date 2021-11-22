4ebd7008601612c36e0fbecd281f5d77b74a82cf
Lucas Oliveira (fptbb) <lucas@fptbb.com>
2021-11-22 11:06:19 -0300

    Adicionado componente pra crafting table

---
+++ src/components/MinecraftCraftingTable.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from './MinecraftCraftingTable.module.css'
+function Item(id) {
+  if (!id || id === '') {
+    return (
+      <div className={styles.gridElement}>
+      </div>
+      );
+  }else{
+    let itemUrl = 'https://minecraftitemids.com/item/32/' + id + '.png';
+    return (
+      <div className={styles.gridElement}>
+          <img className={styles.displayed} src={itemUrl} />
+      </div>
+    )
+  }
+  
+}
+
+export default ({Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8, Item9}) => {
+  return (
+  <div className={styles.grid}>
+      {Item(Item1)}
+      {Item(Item2)}
+      {Item(Item3)}
+      {Item(Item4)}
+      {Item(Item5)}
+      {Item(Item6)}
+      {Item(Item7)}
+      {Item(Item8)}
+      {Item(Item9)}
+  </div>
+  )
+}
