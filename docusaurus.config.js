b45eb9bad9b59c1239be19f81393f1bd37c48504
Lucas Oliveira <lucas@fptbb.com>
2024-04-09 12:12:31 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,6 +70,7 @@ const config = {
           { to: '/blog', label: 'Blog', position: 'left' },
           { to: '/regras', label: 'Regras', position: 'left' },
           { to: '/doador', label: 'VIRE UM DOADOR', position: 'left' },
+          { to: '/termos', label: 'Termos e Privacidade', position: 'left' },
         ],
       },
 
